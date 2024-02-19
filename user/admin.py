ea2dedc4a548d6001fad3b23f12bc4a5f92d1840
Your Name <you@example.com>
2024-02-19 20:27:21 +0600

    apps

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Cart
+from .models import User, MyUser
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Cart)
+admin.site.register(User)
+admin.site.register(MyUser)
